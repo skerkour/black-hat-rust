0ee8ae2fd5465a50940497ffffa35cb5732a98bc
Sylvain Kerkour <sylvain@kerkour.fr>
2021-03-21 17:58:58 +0000

    ch_02/tricoder: Improve usage error

--- ch_02/tricoder/src/error.rs
+++ ch_02/tricoder/src/error.rs
@@ -2,6 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug, Clone)]
 pub enum Error {
-    #[error("Usage: tricoder [target.com]")]
+    #[error("Usage: tricoder <target.com>")]
     CliUsage,
 }
