7cb075d0ee17891b8eee91c76597d6a18dfb7f75
Sylvain Kerkour <sylvain@kerkour.fr>
2021-03-21 15:27:47 +0000

    ch_08: executor

---
+++ ch_08/executor/src/main.rs
@@ -0,0 +1,15 @@
+use std::mem;
+
+// we do this trick because otherwise only the reference is in the .text section
+const SHELLCODE_BYTES: &[u8] = include_bytes!("../../shellcode.bin");
+const SHELLCODE_LENGTH: usize = SHELLCODE_BYTES.len();
+
+#[no_mangle]
+#[link_section = ".text"]
+static SHELLCODE: [u8; SHELLCODE_LENGTH] = *include_bytes!("../../shellcode.bin");
+
+fn main() {
+    let exec_shellcode: extern "C" fn() -> ! =
+        unsafe { mem::transmute(&SHELLCODE as *const _ as *const ()) };
+    exec_shellcode();
+}
