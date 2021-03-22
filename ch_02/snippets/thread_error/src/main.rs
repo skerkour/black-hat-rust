6b28bd50655d5785211bbf1a083df01f577d6cf2
Sylvain Kerkour <sylvain@kerkour.fr>
2021-03-22 11:11:31 +0000

    ch_02: snippets

--- ch_02/snippets/thread_error/src/main.rs
+++ ch_02/snippets/thread_error/src/main.rs
@@ -4,12 +4,11 @@ fn main() {
     let mut my_vec: Vec<i64> = Vec::new();
 
     thread::spawn(move || { // <- notice the move keyword here
+    // thread::spawn(|| {
         add_to_vec(&mut my_vec);
-    })
-    .join()
-    .expect("joining thread");
+    });
 
-    println!("{:?}", &my_vec);
+    my_vec.push(34)
 }
 
 fn add_to_vec(vec: &mut Vec<i64>) {
