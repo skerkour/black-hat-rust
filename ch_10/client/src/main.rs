0a7edba6a076149bb587364634bb4d58acb4bed4
Sylvain Kerkour <sylvain@kerkour.fr>
2021-03-25 11:47:06 +0000

    ch_10: Dockerfile

--- ch_10/client/src/main.rs
+++ ch_10/client/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let cli = App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .about(clap::crate_description!())
+        .subcommand(SubCommand::with_name("list").about("List all activre agents"))
+        .setting(clap::AppSettings::ArgRequiredElseHelp)
+        .setting(clap::AppSettings::DisableVersion)
+        .setting(clap::AppSettings::VersionlessSubcommands)
+        .get_matches();
 }
