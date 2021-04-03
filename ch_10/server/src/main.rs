ea7c567dfa55137dd73d516e8e3a42c35683b0d1
Sylvain Kerkour <sylvain@kerkour.fr>
2021-04-03 10:38:31 +0000

    ch_10/server: Start config

--- ch_10/server/src/main.rs
+++ ch_10/server/src/main.rs
@@ -1,5 +1,6 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware, web, App, HttpServer};
 
+mod config;
 mod routes;
 
 #[actix_web::main]
@@ -8,9 +9,13 @@ async fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
 
     HttpServer::new(|| {
-        App::new().service(web::resource("/{id}/{name}").route(web::get().to(routes::index)))
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/{id}/{name}").route(web::get().to(routes::index)))
     })
     .bind("127.0.0.1:8080")?
     .run()
-    .await
+    .await?;
+
+    Ok(())
 }
