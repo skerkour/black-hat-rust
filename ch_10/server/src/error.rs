28b45391c6848f1e0b0e75a72945b897fcaa4931
Sylvain Kerkour <sylvain@kerkour.fr>
2021-04-03 13:53:55 +0000

    ch10: config

---
+++ ch_10/server/src/error.rs
@@ -0,0 +1,23 @@
+use thiserror::Error;
+
+#[derive(Error, Debug, Clone)]
+pub enum Error {
+    #[error("Internal error")]
+    Internal(String),
+    #[error("{0}")]
+    NotFound(String),
+    #[error("{0}")]
+    InvalidArgument(String),
+}
+
+impl std::convert::From<sqlx::migrate::MigrateError> for Error {
+    fn from(err: sqlx::migrate::MigrateError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl std::convert::From<std::num::ParseIntError> for Error {
+    fn from(err: std::num::ParseIntError) -> Self {
+        Error::InvalidArgument(err.to_string())
+    }
+}
--- ch_10/server/src/main.rs
+++ ch_10/server/src/main.rs
@@ -1,19 +1,27 @@
 use actix_web::{middleware, web, App, HttpServer};
 
 mod config;
+mod error;
 mod routes;
+
+use config::Config;
+pub use error::Error;
 
 #[actix_web::main]
 async fn main() -> Result<(), anyhow::Error> {
     ::std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
     env_logger::init();
 
+    let config = Config::load()?;
+
+    let addr = format!("0.0.0.0:{}", config.port);
+
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
             .service(web::resource("/{id}/{name}").route(web::get().to(routes::index)))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(&addr)?
     .run()
     .await?;
 
