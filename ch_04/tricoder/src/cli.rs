c2e653a6a829d4033c266325fe2023b1f2e7aff4
Sylvain Kerkour <sylvain@kerkour.com>
2021-07-06 17:30:40 +0000

    ch_04/tricoder: remove Arc<HttpClient>

--- ch_04/tricoder/src/cli.rs
+++ ch_04/tricoder/src/cli.rs
@@ -3,7 +3,6 @@ use futures::StreamExt;
 use reqwest::Client;
 use std::collections::HashSet;
 use std::iter::FromIterator;
-use std::sync::Arc;
 use std::time::Duration;
 use std::time::Instant;
 
@@ -38,7 +37,7 @@ pub fn scan(target: &str) -> Result<(), Error> {
         .expect("Building tokio's runtime");
 
     let http_timeout = Duration::from_secs(10);
-    let http_client = Arc::new(Client::builder().timeout(http_timeout).build()?);
+    let http_client = Client::builder().timeout(http_timeout).build()?;
     let dns_resolver = dns::new_resolver();
 
     let subdomains_concurrency = 20;
@@ -162,55 +161,3 @@ pub fn scan(target: &str) -> Result<(), Error> {
 
     Ok(())
 }
-
-// let args: Vec<String> = env::args().collect();
-
-// if args.len() != 2 {
-//     return Err(Error::CliUsage.into());
-// }
-
-// let target = args[1].as_str();
-
-// let http_timeout = Duration::from_secs(10);
-// let http_client = Client::builder().timeout(http_timeout).build()?;
-
-// let ports_concurrency = 200;
-// let subdomains_concurrency = 100;
-// let scan_start = Instant::now();
-
-// let scan_result = runtime.block_on(async move {
-//     let subdomains = subdomains::enumerate(&http_client, target).await?;
-
-//     // Concurrent stream method 1: Using an Arc<Mutex<T>>
-//     let res: Arc<Mutex<Vec<Subdomain>>> = Arc::new(Mutex::new(Vec::new()));
-
-//     stream::iter(subdomains.into_iter())
-//         .for_each_concurrent(subdomains_concurrency, |subdomain| {
-//             let res = res.clone();
-//             async move {
-//                 let subdomain = ports::scan_ports(ports_concurrency, subdomain).await;
-//                 res.lock().await.push(subdomain)
-//             }
-//         })
-//         .await;
-
-//     Ok::<_, crate::Error>(
-//         Arc::try_unwrap(res)
-//             .expect("Moving out from subdomains Arc")
-//             .into_inner(),
-//     )
-// })?;
-
-// let scan_duration = scan_start.elapsed();
-// info!("Scan completed in {:?}", scan_duration);
-
-// info!("Found {} subdomains", scan_result.len());
-
-// for subdomain in scan_result {
-//     println!("{}:", &subdomain.domain);
-//     for port in &subdomain.open_ports {
-//         println!("    {}: open", port.port);
-//     }
-
-//     println!("");
-// }
