e0dcc0cdedaf5667a16ff61008d69b520fac3aba
Sylvain Kerkour <sylvain@kerkour.com>
2021-07-06 17:05:12 +0000

    ch_04/tricoder: Improve port scanning

--- ch_04/tricoder/src/ports.rs
+++ ch_04/tricoder/src/ports.rs
@@ -2,45 +2,28 @@ use crate::{
     common_ports::MOST_COMMON_PORTS_100,
     modules::{Port, Subdomain},
 };
-use futures::StreamExt;
+use futures::{stream, StreamExt};
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::time::Duration;
 use tokio::net::TcpStream;
-use tokio::sync::mpsc;
 
-pub async fn scan_ports(concurrency: usize, subdomain: Subdomain) -> Subdomain {
-    let mut ret = subdomain.clone();
+pub async fn scan_ports(concurrency: usize, mut subdomain: Subdomain) -> Subdomain {
+    let hostname = &subdomain.domain.clone();
 
-    // Concurrent stream method 2: using channels
-    let (input_tx, input_rx) = mpsc::channel(concurrency);
-    let (output_tx, output_rx) = mpsc::channel(concurrency);
-
-    tokio::spawn(async move {
-        for port in MOST_COMMON_PORTS_100 {
-            let _ = input_tx.send(*port).await;
-        }
-    });
-
-    let input_rx_stream = tokio_stream::wrappers::ReceiverStream::new(input_rx);
-    input_rx_stream
-        .for_each_concurrent(concurrency, |port| {
-            let subdomain = subdomain.clone();
-            let output_tx = output_tx.clone();
-            async move {
-                let port = scan_port(&subdomain.domain, port).await;
-                if port.is_open {
-                    let _ = output_tx.send(port).await;
-                }
+    subdomain.open_ports = stream::iter(MOST_COMMON_PORTS_100.into_iter())
+        .map(|port| async move {
+            let port = scan_port(hostname, *port).await;
+            if port.is_open {
+                return Some(port);
             }
+            None
         })
+        .buffer_unordered(concurrency)
+        .filter_map(|port| async { port })
+        .collect()
         .await;
-    // close channel
-    drop(output_tx);
 
-    let output_rx_stream = tokio_stream::wrappers::ReceiverStream::new(output_rx);
-    ret.open_ports = output_rx_stream.collect().await;
-
-    ret
+    subdomain
 }
 
 async fn scan_port(hostname: &str, port: u16) -> Port {
