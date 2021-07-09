8516aa9226b8d6c7fb44787de09c5e19853f01e3
Sylvain Kerkour <sylvain@kerkour.com>
2021-07-09 08:05:49 +0000

    ch_03/snippets: Add concurrent_stream

---
+++ ch_03/snippets/concurrent_stream/src/main.rs
@@ -0,0 +1,15 @@
+use futures::{stream, StreamExt};
+use rand::{thread_rng, Rng};
+use std::time::Duration;
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() {
+    stream::iter(0..200u64)
+        .for_each_concurrent(20, |number| async move {
+            let mut rng = thread_rng();
+            let sleep_ms: u64 = rng.gen_range(0..20);
+            tokio::time::sleep(Duration::from_millis(sleep_ms)).await;
+            println!("{}", number);
+        })
+        .await;
+}
