6b7973264fc5d991faf92659fd52a16208b8f68c
Sylvain Kerkour <sylvain@kerkour.com>
2021-10-06 09:35:41 +0000

    ch_05/snippets: Add atomic

--- ch_05/snippets/atomic/src/main.rs
+++ ch_05/snippets/atomic/src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
+use std::thread;
 
 fn main() {
     // creating a new atomic
@@ -8,20 +9,24 @@ fn main() {
     // adding 1
     my_atomic.fetch_add(1, Ordering::SeqCst);
 
-
     // geting the value
     assert!(my_atomic.load(Ordering::SeqCst) == 43);
 
     // substracting 1
     my_atomic.fetch_sub(1, Ordering::SeqCst);
 
-
     // replacing the value
     my_atomic.store(10, Ordering::SeqCst);
+    assert!(my_atomic.load(Ordering::SeqCst) == 10);
 
     // other avalable operations
     // fetch_xor, fetch_or, fetch_nand, fetch_and...
 
     // creating a new atomic that can be shared between threads
     let my_arc_atomic = Arc::new(AtomicUsize::new(4));
+
+    let second_ref_atomic = my_arc_atomic.clone();
+    thread::spawn(move|| {
+        second_ref_atomic.store(42, Ordering::SeqCst);
+    });
 }
